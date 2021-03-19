09513c502ad6d2c4f5144e48204a71a2a60479b8
Jon W <jonwaaler@outlook.com>
2021-03-18 23:02:56 -0400

    changed form to  use radio input

---
+++ assets/java/java.js
@@ -0,0 +1,15 @@
+// 1. at start load in the first question
+// repeat {
+// 2. user selects question and submits the form
+// 3. answer is stored
+// 4. next question loads
+// }
+// after last question is submitted show results
+
+/* QUIZ FORM
+    <h6>What is not a valid variable type in JavaScript</h6>
+    <button>number</button>
+    <button>string</button>
+    <button>float</button>
+    <button>bool</button>
+*/
